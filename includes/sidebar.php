cc535d8a74bee6f9de455f767812c55b23b73e58
It's not working at all! <itsnotworkingatall@gmail.com>
2017-07-29 17:58:24 +0300

    login form on frontend

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -32,7 +32,22 @@
         <!-- /.input-group -->
     </div>
     
-   
+    <!-- Blog Login Well -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter user name">
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter password">
+                <span class="input-group-btn">
+                   <button class="btn btn-primary" name="login" type="submit">Login</button>                   
+                </span>
+            </div>
+        </form> 
+        <!-- /.input-group -->
+    </div>
     
 
     <!-- Blog Categories Well -->
@@ -52,9 +67,11 @@
                         while($row = mysqli_fetch_assoc($selectAllCategories)){
                             $categoryId = $row['cat_id'];
                             $categoryTitle = $row['cat_title'];
-                            echo "<li><a href='category.php?c_id={$categoryId}'>{$categoryTitle}</a></li>"; 
-                        }
                     ?>
+                            <li><a href='category.php?c_id=<?php echo $categoryId ?>'><?php echo $categoryTitle ?></a></li>
+                    <?php 
+                        } //end of while loop
+                        ?>
 
                     </ul>
             </div>           
