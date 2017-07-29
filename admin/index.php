d7cc2a0790ce11e5dfc4b4e6b238bd2f30ffcba1
It's not working at all! <itsnotworkingatall@gmail.com>
2017-07-29 19:26:51 +0300

    login to admin panel
    login to admin panel from frontend.

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,5 @@
 <?php include "includes/admin_header.php" ?>
 <div id="wrapper">
-<?php if(!$connectionToDB) echo "<h1>Connection to DB failed</h1>"; ?>
 <?php include "includes/admin_navigation.php" ?>     
 <div id="page-wrapper">
     <div class="container-fluid">
@@ -8,8 +7,9 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Welcome to admin
-                    <small>Author</small>
+                    Welcome to admin <?php //echo $_SESSION['userrole'] ?>
+                    
+                    <small><?php echo $_SESSION['username'] ?></small>
                 </h1>
               <?php //include "includes/breadcrumbs.php" ?>  
               <div>
---
+++ includes/login.php
@@ -0,0 +1,43 @@
+<?php include "db.php" ?>
+<?php include "../admin/includes/functions.php" ?>
+<?php session_start() ?>
+
+<?php
+
+if (isset($_POST['login'])) {
+    $userName     = $_POST['username'];
+    $userPassword = $_POST['password'];
+}
+
+$userName = mysqli_real_escape_string($connectionToDB, $userName);
+$userPassword = mysqli_real_escape_string($connectionToDB, $userPassword);
+
+$query = "SELECT * FROM users WHERE user_name = '{$userName}' ";
+$selectUser = mysqli_query($connectionToDB, $query);
+confirmQuery($selectUser);
+    
+while ($row = mysqli_fetch_assoc($selectUser)) {
+    $dbId       = $row['user_id'];
+    $dbUName    = $row['user_name'];
+    $dbFName    = $row['user_first_name'];
+    $dbLName    = $row['user_last_name'];
+    $dbPassword = $row['user_password'];
+    $dbRole     = $row['user_role'];
+    $dbStatus   = $row['user_status'];
+}
+
+if ($userName === $dbUName && $userPassword === $dbPassword) {
+    
+    $_SESSION['username'] = $dbUName;
+    $_SESSION['userfirstname'] = $dbFName;
+    $_SESSION['userlastname'] = $dbLName;
+    $_SESSION['userrole'] = $dbRole;
+    $_SESSION['userstatus'] = $dbStatus;
+    
+    header("Location: ../admin");
+    
+} else {
+    
+    header("Location: ../index.php");
+    
+}
